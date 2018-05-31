513271311b2048c7b6dffca222c744167212614d
Moein <tje3d@yahoo.com>
2018-06-01 00:06:43 +0430

    Force LTR

--- android/app/src/main/java/com/ekram/MainApplication.java
+++ android/app/src/main/java/com/ekram/MainApplication.java
@@ -7,6 +7,7 @@ import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
@@ -43,5 +44,9 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+
+    I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
+    sharedI18nUtilInstance.forceRTL(this, false);
+    sharedI18nUtilInstance.allowRTL(this, false);
   }
 }
